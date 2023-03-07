04896fc677b8ef5c6859ba1d290dc4e320d4f12e
nomomon <m.nurmukhambetov@researchable.nl>
2023-03-07 19:37:28 +0100

    remove spaces from code

--- components/parseRoomNumber.ts
+++ components/parseRoomNumber.ts
@@ -1,10 +1,15 @@
 import Room from "./interface/Room";
 
-const parseRoomNumber = (room: Room) => {
+const a = (room: Room) => {
     const { code, name: { nl: name } } = room;
     if (!code.startsWith("#")) return code;
     if (name.startsWith("BB")) return name.slice(2, 12);
     return name.slice(0, 10);
+};
+
+const parseRoomNumber = (room: Room) => {
+    const roomCode = a(room);
+    return roomCode.replaceAll(" ", "")
 };
 
 export default parseRoomNumber;
