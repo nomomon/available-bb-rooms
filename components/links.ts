eda8c63e33eab8da5bcfd0b3ac89c4f9e9520d52
nomomon <funisinfinity26@gmail.com>
2023-09-18 13:34:25 +0200

    update data to 2023-2024

--- components/links.ts
+++ components/links.ts
@@ -1,7 +1,7 @@
-const LAB_ACTIVITY = "https://rooster.rug.nl/api/v2/2022-2023/activity/by/building/sebernoulliborgcomputer";
-const LAB_ROOMS = "https://rooster.rug.nl/api/2022-2023/locationUnit/by/building/sebernoulliborgcomputer"
-const LECTURE_ACTIVITY = "https://rooster.rug.nl/api/v2/2022-2023/activity/by/building/sebernoulliborglecture"
-const LECTURE_ROOMS = "https://rooster.rug.nl/api/2022-2023/locationUnit/by/building/sebernoulliborglecture"
+const LAB_ACTIVITY = "https://rooster.rug.nl/api/v2/2023-2024/activity/by/building/sebernoulliborgcomputer";
+const LAB_ROOMS = "https://rooster.rug.nl/api/2023-2024/locationUnit/by/building/sebernoulliborgcomputer"
+const LECTURE_ACTIVITY = "https://rooster.rug.nl/api/v2/2023-2024/activity/by/building/sebernoulliborglecture"
+const LECTURE_ROOMS = "https://rooster.rug.nl/api/2023-2024/locationUnit/by/building/sebernoulliborglecture"
 
 const links = {
     LAB_ACTIVITY,
